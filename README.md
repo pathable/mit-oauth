a1fe4bd414af4c2b8b7f3221ae325edbfe559cbf
Luiz <luizcarlos1405@gmail.com>
2020-09-02 13:40:44 -0300

    Package description updated to format
    Removing github from README

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# github-oauth
+# mit-oauth
 
-An implementation of the GitHub OAuth flow. See the [Meteor Guide](https://guide.meteor.com/accounts.html) for more details.
--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
-  name: 'pathable:mit-oauth',
   summary: 'Mit OAuth flow',
   version: '1.0.0',
+  name: 'pathable:mit-oauth',
+  git: 'https://github.com/pathable/mit-oauth',
 });
 
 Package.onUse(api => {
+  api.versionsFrom('1.11')
+
   api.use('ecmascript', ['client', 'server']);
   api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
+  api.use('pathable:oauth@1.3.1', ['client', 'server']);
   api.use('http', 'server');
   api.use('random', 'client');
   api.use('service-configuration', ['client', 'server']);
