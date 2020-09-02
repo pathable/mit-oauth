d8e1d049aea107b743e3d8b2120798c942bbf50b
Igor Fontana <igor193@gmail.com>
2020-09-02 15:03:12 -0300

    bumping versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Mit OAuth flow',
-  version: '1.0.0',
+  version: '1.0.1',
   name: 'pathable:mit-oauth',
   git: 'https://github.com/pathable/mit-oauth',
 });
@@ -9,7 +9,7 @@ Package.onUse(api => {
   api.versionsFrom('1.11')
 
   api.use('ecmascript', ['client', 'server']);
-  api.use('oauth2', ['client', 'server']);
+  api.use('pathable:oauth2@1.3.1', ['client', 'server']);
   api.use('pathable:oauth@1.3.1', ['client', 'server']);
   api.use('http', 'server');
   api.use('random', 'client');
